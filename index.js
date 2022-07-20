f88bafcd2fa43ecf76c55b08351225862085e161
Paul Jung <pbnjcub@gmail.com>
2022-07-20 08:20:04 -0400

    lab complete but did not pass second objective, although what I wrote worked

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((responseJson) => {
+      let books = responseJson.map(({name}) => ({name}))
+      renderBooks(books)
+    })
+  }
   
-}
 
 function renderBooks(books) {
   const main = document.querySelector('main');
